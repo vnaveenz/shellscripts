46a4eb291516827e9e7196ba15a6d4c936f1016d
naveen <naveenblaze44@gmail.com>
2020-04-22 16:49:26 -0400

    better version

--- sshkeygen.sh
+++ sshkeygen.sh
@@ -13,13 +13,13 @@ read -p 'Do you need passphrase (Y/N)?: ' passoption
 
 echo $passoption
 
-while [[ $passoption != "Y" && $passoption != "y" && $passoption != "N" && $passoption != "n" ]]
+while [[ $passoption != [YyNn] ]]
 do 
     echo 'incorrect option' 
     read -p 'Do you need passphrase (Y/N)?: ' passoption
 done
 
-if [[ $passoption == 'Y' || $passoption == 'y' ]]
+if [[ $passoption == [Yy] ]]
 then
     read -p  'Please enter the passphrase of your wish! ' -s passphrase
     ssh-keygen -b 2048 -t rsa -f $HOME/.ssh/$keyname -q -N "$passphrase"
